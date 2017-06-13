babb528e1f0e19f638288838851056c0865666f6
lingjinjiang <lingjinjiang@gmail.com>
2017-06-13 22:19:09 +0800

    add output parameter

--- src/main/java/org/ling/kafka/kafkaConnectAsConsumer.java
+++ src/main/java/org/ling/kafka/kafkaConnectAsConsumer.java
@@ -2,14 +2,17 @@ package org.ling.kafka;
 
 import java.util.Properties;
 
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
-public class kafkaConnectAsConsumer {
+public class kafkaConnectAsConsumer{
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception{
+
+        final ParameterTool params = ParameterTool.fromArgs(args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment
                 .getExecutionEnvironment();
 
@@ -21,6 +24,11 @@ public class kafkaConnectAsConsumer {
         
         DataStream<String> stream = env.addSource(new FlinkKafkaConsumer010("topic", new SimpleStringSchema(), properties));
 
-        stream.print();
+        if (params.has("output")) {
+            stream.writeAsText(params.get("output"));
+        } else {
+            stream.print();
+        }
+        env.execute();
     }
 }
